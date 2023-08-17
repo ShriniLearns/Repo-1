345ed60b21cb00db55bae382ed919ef1e0c825bc
ShriniLearns <138767179+ShriniLearns@users.noreply.github.com>
2023-08-17 11:57:14 +0530

    Update Script1.sh

--- Scripts/Script1.sh
+++ Scripts/Script1.sh
@@ -1,9 +1,15 @@
+set -e
+
+REPO_OWNER=$1
+REPO_NAME=$2
+REPO_TOKEN=$3
+
 upload_artifacts(){
   echo "Report Name is : $1"
   curl -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer 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"\
+  -H "Authorization: Bearer $REPO_TOKEN"\
   -H "X-GitHub-Api-Version: 2022-11-28" \
 https://api.github.com/repos/ShriniLearns/GHA-GitHubActions/actions/workflows/upload_artifact.yml/dispatches \
   -d '{"ref":"main","inputs":{"reportName":"'"$1"'"}}'
